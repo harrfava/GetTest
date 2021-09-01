028aa093226564c4859b33f813e3f643dfa58718
harrfava <harrfava@ut.utm.edu>
2021-09-01 17:23:17 -0500

    My Guessing Game....Took me forever to show number of attempts, but we figured it out!

--- Test1Project/Program.cs
+++ Test1Project/Program.cs
@@ -10,7 +10,50 @@ namespace Test1Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! ");
+            Random n = new Random();
+            int CorrectNum = n.Next(0, 100);
+            int NumofGuess = 0; 
+            bool win = false;
+
+            do
+            {
+                Console.WriteLine("I thinking of a Number from 0 - 100: ");
+                
+                string r = Console.ReadLine();
+
+                int i = int.Parse(r);
+
+                if (i > CorrectNum)
+                {
+                    Console.WriteLine("To high!");
+                    NumofGuess++;
+
+
+                }
+                else if (i < CorrectNum)
+                {
+                    Console.WriteLine("To Low!");
+                    NumofGuess++;
+                }
+
+                else if (i == CorrectNum)
+                {
+                    Console.WriteLine("Correct! ");
+                    NumofGuess++;
+                    win = true;
+
+                }
+            } 
+            while (win == false);
+
+            Console.WriteLine("Number of Guesses: " + NumofGuess);
+
+            Console.Write("Press any button to exit: " );
+            Console.ReadKey(true);
+
+
         }
+
     }
-}
+
+}     
